9bef7198b0493a6bee4ecef10a98460a5ecd2473
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-12-09 20:01:48 -0500

    added POST test but need to fix

--- server/server.js
+++ server/server.js
@@ -13,6 +13,24 @@ app.get("/", (req, res) => {
   res.send("Server works");
 });
 
+app.post("/api/users", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  const newUser = {
+    id: 1,
+    username,
+    password,
+  };
+
+  res.status(201).json({ message: "User created successfully", user: newUser });
+});
+
 const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
